5c527f7f45e144f089de7c3fdf11e0fcb66b4482
Hologram542 <hologram542@gmail.com>
2023-02-01 13:55:48 +0530

    k

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ with but_col2:
 # If the "SPEED STIMULATION" is pressed
 if speed_stimulation:
   st.markdown("""Time spent = {0}\nsource of pollution (cx, cy) = {1}, {2}\nu = {3}, v = {4}""".format(time_step_dropdown, cx_dropdown, cy_dropdown, u_input, v_input))
-  # demo_client.generate_PINN_model(time_step_dropdown, cx_dropdown, cy_dropdown,u_input,v_input)
+  generate_PINN_model(time_step_dropdown, cx_dropdown, cy_dropdown,u_input,v_input)
+  st.image('pinn model.png')
   # math_model.generate_math_model(cx_dropdown, cy_dropdown,u_input,v_input, time_step_dropdown)
   st.write("Time to generate PINN model - 1481.868 milliseconds")
