49cb8de5c20c1e98a6a5c23fcf11c800afa8db23
Hologram542 <hologram542@gmail.com>
2023-02-02 23:51:21 +0530

    b

--- math_model.py
+++ math_model.py
@@ -26,7 +26,7 @@ def scale(num,min,max):
   num_scaled = (num-middle)/(max-middle)
   return num_scaled
 
-@jit
+@jit(nogil=True)
 def run_code(total_rows,total_columns,f, phi_old,obstructions, u, v, time_steps):
   #def scale(num,min,max):
     #middle = (min+max)/2
