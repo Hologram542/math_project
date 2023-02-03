0997c7bb670f12f770663c46fe1da8c1a7bffb3d
Hologram542 <hologram542@gmail.com>
2023-02-03 17:00:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown("<h1 style='text-align: center;font-size:15px'>SIMPLE FLOW REGIME WI
 def run_models(time_step, cx, cy, u, v, show_input = True):
 
   if show_input:
-    st.write('<style>div.block-container{padding-bottom:1rem;}</style>', unsafe_allow_html=True)
+
     st.markdown("""<h1 style='text-align: center;font-size:25px'>Results</h1>""".format(time_step, cx, cy, u, v), unsafe_allow_html=True)
     st.markdown("""<h1 style='text-align: center;font-size:15px'>Time spent = {0}, source of pollution (cx, cy) = {1}, {2}, u = {3}, v = {4}</h1>""".format(time_step, cx, cy, u, v), unsafe_allow_html=True)
   
@@ -101,8 +101,8 @@ def accuracy_func_ui(time_steps_max):
 
 ## Dropdowns
 
-st.write('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 st.write("**Enter the values for simulation**")
+st.markdown("""<style>[data-baseweb="select"] {margin-top: -50px;}</style>""",unsafe_allow_html=True,)
 time_step_dropdown = st.selectbox("Time step", np.arange(100, 1100, 100), index = 9)
 
 st.write('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
