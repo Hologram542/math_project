02015287da5e958de95e617dbd596f971904d8b9
Hologram542 <hologram542@gmail.com>
2023-02-03 20:17:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def accuracy_func_ui(time_steps_max):
 ## Dropdowns
 
 st.write("**Enter the values for simulation**")
-st.markdown("""<style>[data-testid="stMarkdownContainer"] {margin-top: -20px;}</style>""",unsafe_allow_html=True)
+st.markdown("""<style>[data-testid="stMarkdownContainer"] {margin-top: -10px;margin-bottom: -10px;}</style>""",unsafe_allow_html=True)
 time_step_dropdown = st.selectbox("Time step", np.arange(100, 1100, 100), index = 9)
 
 
