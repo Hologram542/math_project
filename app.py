13a67133f81510782094c64459875235d0bccf09
Hologram542 <hologram542@gmail.com>
2023-02-03 16:14:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,11 +63,11 @@ def performance_func_ui(pinn_time, math_time, initial_fig, pinn_fig, math_fig):
 
   with img_col2:
     st.image('pinn model.png')
-    st.write("Time to generate PINN model **{:.{3}f}** milliseconds".format(str(pinn_time)))
+    st.write("Time to generate PINN model **{}** milliseconds".format(str(round(pinn_time, 3)))
 
   with img_col3:
     st.image('math model.png')
-    st.write("Time to generate math model **{:.{3}f}** milliseconds".format(str(math_time)))
+    st.write("Time to generate math model **{}** milliseconds".format(str(round(math_time, 3))))
 
 def accuracy_func_ui(time_steps_max):
   
