243b96a9065de08fe662ee8ab2a8ec9867b9415a
Hologram542 <hologram542@gmail.com>
2023-02-04 03:37:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,12 +156,12 @@ def accuracy_func_ui(time_step_dropdown, cx_dropdown, cy_dropdown, u_dropdown, v
 
 ## Dropdowns
 
-st.markdown("<h1 style='text-align: center;font-size:20px;padding-top:rem0;'>Enter the values for simulation</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-size:20px;padding-top:rem0;padding-bottom:rem0'>Enter the values for simulation</h1>", unsafe_allow_html=True)
 
 time_step_dropdown = st.selectbox("Time step", np.arange(100, 1100, 100), index = 9)
 
 
-st.markdown("<h1 style='font-size:15px;padding-top:rem0;'>Pollution Source Location</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-size:15px;padding-top:rem0;padding-bottom:rem0'>Pollution Source Location</h1>", unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 with col1:
   cx_dropdown = st.selectbox("cx", np.arange(0, 110, 10), index = 6)
@@ -169,7 +169,7 @@ with col1:
 with col2:
   cy_dropdown = st.selectbox("cy", np.arange(0, 110, 10), index = 6)
 
-st.markdown("<h1 style='font-size:15px;padding-top:rem0;'>velocity in x and y directions</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-size:15px;padding-top:rem0;padding-bottom:rem0'>velocity in x and y directions</h1>", unsafe_allow_html=True)
 col3, col4 = st.columns(2)
 
 with col3:
