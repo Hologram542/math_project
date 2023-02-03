1ef15d1b31ee0c6d6db0c72ce2b878d38e3fcc50
Hologram542 <hologram542@gmail.com>
2023-02-03 18:04:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def performance_func_ui(pinn_time, math_time, initial_fig, pinn_fig, math_fig):
   with lower_col3:
     st.markdown("<h1 style='text-align: center;font-size:20px'>Ground Truth (Math Model Results)</h1>", unsafe_allow_html=True)
     st.image('math model.png')
-    st.markdown(f"<h1 style='text-align: center;font-size:15px'>Time to generate PINN model {str(round(math_time, 3))} milliseconds</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h1 style='text-align: center;font-size:15px'>Time to generate math model {str(round(math_time, 3))} milliseconds</h1>", unsafe_allow_html=True)
 
 def accuracy_func_ui(time_steps_max):
   
