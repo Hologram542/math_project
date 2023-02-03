81e99fb3c29c48c066ea2f20935f79a832694afb
Hologram542 <hologram542@gmail.com>
2023-02-04 03:22:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(layout="wide")
 st.write('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 #Title
 st.markdown("<h1 style='text-align: center;font-size:20px'>SIMPLE FLOW REGIME WITH CONSTANT ADVECTION VALUES </h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center;font-size:20px'>(2D Advection Diffusion Pollution Spread Math Model and PINN) </h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;font-size:20px;padding-top:0rem;'>(2D Advection Diffusion Pollution Spread Math Model and PINN) </h1>", unsafe_allow_html=True)
 
 
 #Run the initial model
