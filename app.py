7c5e8fb0d51e5190e6abf052c3d449a8749c2205
Hologram542 <hologram542@gmail.com>
2023-02-04 01:10:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def accuracy_func_ui(time_step_dropdown, cx_dropdown, cy_dropdown, u_dropdown, v
 
   # num_cols = ((time_step_dropdown)//100) + 2
 
-  num_cols = st.number_input()
+  num_cols = st.number_input('number')
 
   pinn_cols = st.columns(num_cols)
   math_cols = st.columns(num_cols)
